41f825f088fc4bfa92930a2a9a6781a54ca2720f
nekojess1 <al.souza.jessica@gmail.com>
2023-07-04 23:18:35 -0300

    :sparkles: integrate login with firebase

--- app/src/main/java/org/nekojess/nutriease/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/org/nekojess/nutriease/ui/onboarding/OnboardingActivity.kt
@@ -17,7 +17,6 @@ class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         setViewPager()
     }
 

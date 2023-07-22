1ecffcbc55e9f6bc299d273f4e2f6e22b537e28e
nekojess1 <al.souza.jessica@gmail.com>
2023-07-22 20:35:08 -0300

    :recycle: change app home

--- app/src/main/java/org/nekojess/nutriease/ui/home/HomeActivity.kt
+++ app/src/main/java/org/nekojess/nutriease/ui/home/HomeActivity.kt
@@ -2,40 +2,16 @@ package org.nekojess.nutriease.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import com.google.android.material.navigation.NavigationBarView
-import org.nekojess.nutriease.R
 import org.nekojess.nutriease.databinding.ActivityHomeBinding
-import org.nekojess.nutriease.ui.configuration.ConfigurationFragment
 
 class HomeActivity : AppCompatActivity() {
     private val binding: ActivityHomeBinding by lazy {
         ActivityHomeBinding.inflate(layoutInflater)
     }
 
-    private val onItemSelectedListener =
-        NavigationBarView.OnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.settings -> {
-                    val configurationFragment = ConfigurationFragment()
-                    replaceFragment(configurationFragment)
-                    return@OnItemSelectedListener true
-                }
-                else -> return@OnItemSelectedListener false
-            }
-        }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.bottomNavigation.setOnItemSelectedListener(onItemSelectedListener)
-        replaceFragment(ConfigurationFragment())
-        binding.bottomNavigation.selectedItemId = R.id.settings
     }
 
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.content_frame, fragment)
-            .commit()
-    }
 }
